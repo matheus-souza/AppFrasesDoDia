22391af6525718bdc2211ee7053c8bf890cb175a
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-04-17 23:51:45 -0300

    Trabalhando com elementos visuais da aplicação

--- index.android.js
+++ index.android.js
@@ -1,53 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react';
+import { Text, View, Button, AppRegistry } from 'react-native';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-export default class AppFrasesDoDia extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+const Style = {
+  textStyle: {
+    fontSize: 40,
+    backgroundColor: '#4bad32',
+    height: 60,
+    width: 60
+  },
+  viewStyle: {
+    backgroundColor: 'skyblue',
+    height: 300,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    alignItems: 'flex-end'
+  }
+};
 
-AppRegistry.registerComponent('AppFrasesDoDia', () => AppFrasesDoDia);
+const App = () => {
+  const { textStyle, viewStyle } = Style;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}></Text>
+    </View>
+  );
+};
+
+AppRegistry.registerComponent('AppFrasesDoDia', () => App);
