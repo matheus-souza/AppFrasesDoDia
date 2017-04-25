f5b2a2f64592ae1fa6aa18bb6a24926a0dd7f6cb
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-04-24 23:21:50 -0300

    Adicionando imagens na aplicação com caminho relativo

--- index.android.js
+++ index.android.js
@@ -1,40 +1,21 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, AppRegistry } from 'react-native';
+import { Text, View, Image, AppRegistry } from 'react-native';
 
 const Style = {
   main: {
     paddingTop: 40
-  },
-  button: {
-    backgroundColor: '#48BBEC',
-    padding: 10,
-    borderColor: '#1d8eb8',
-    borderWidth: 1,
-    borderRadius: 8,
-    shadowColor: '#000',
-    shadowOffset: {width:0, height:2},
-    shadowOpacity: 0.4
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-    alignSelf: 'center'
   }
 };
 
-const botaoPressionado = () => {
-  alert("Botão pressionado");
-}
 
 const App = () => {
-  const { main, button, buttonText } = Style;
+  const { main } = Style;
 
   return (
     <View style={main}>
-      <TouchableOpacity style={button}>
-        <Text style={buttonText}>Clique aqui</Text>
-      </TouchableOpacity>
+      <Image
+        source={ require('./imgs/test.png') }
+      />
     </View>
   );
 };
