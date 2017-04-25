197bbe57fb209631d5cbdff8e5909fcfc8a3f98a
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-04-24 22:17:01 -0300

    Criando layout proporcional com o atributo flex

--- index.android.js
+++ index.android.js
@@ -2,26 +2,32 @@ import React from 'react';
 import { Text, View, Button, AppRegistry } from 'react-native';
 
 const Style = {
-  textStyle: {
-    fontSize: 40,
-    backgroundColor: '#4bad32',
-    height: 60,
-    width: 60
+  main: {
+    flex: 1,
+    backgroundColor: 'cornflowerblue'
   },
-  viewStyle: {
-    backgroundColor: 'skyblue',
-    height: 300,
-    justifyContent: 'center',
-    alignItems: 'flex-end'
+  top: {
+    flex: 2,
+    backgroundColor: 'brown'
+  },
+  content: {
+    flex: 8,
+    backgroundColor: 'darkblue'
+  },
+  footer: {
+    flex: 1,
+    backgroundColor: 'orangered'
   }
 };
 
 const App = () => {
-  const { textStyle, viewStyle } = Style;
+  const { main, top, content, footer } = Style;
 
   return (
-    <View style={viewStyle}>
-      <Text style={textStyle}></Text>
+    <View style={main}>
+      <Text style={top}>Topo</Text>
+      <Text style={content}>Conteudo</Text>
+      <Text style={footer}>Rodape</Text>
     </View>
   );
 };
