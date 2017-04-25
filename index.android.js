2d19becc595e497e8815725c236391c63ed437d3
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-04-24 23:07:07 -0300

    Testando TouchableOpacity

--- index.android.js
+++ index.android.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { Text, View, Button, AppRegistry } from 'react-native';
+import { Text, View, TouchableOpacity, AppRegistry } from 'react-native';
 
 const Style = {
   main: {
     paddingTop: 40
+  },
+  button: {
+    backgroundColor: '#48BBEC',
+    padding: 10,
+    borderColor: '#1d8eb8',
+    borderWidth: 1,
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: {width:0, height:2},
+    shadowOpacity: 0.4
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    alignSelf: 'center'
   }
 };
 
@@ -12,16 +28,13 @@ const botaoPressionado = () => {
 }
 
 const App = () => {
-  const { main } = Style;
+  const { main, button, buttonText } = Style;
 
   return (
     <View style={main}>
-      <Button
-        onPress={botaoPressionado}
-        title="Clique aqui"
-        color="#841584"
-        accessabilityLabel="Clique para visualizar"
-      />
+      <TouchableOpacity style={button}>
+        <Text style={buttonText}>Clique aqui</Text>
+      </TouchableOpacity>
     </View>
   );
 };
