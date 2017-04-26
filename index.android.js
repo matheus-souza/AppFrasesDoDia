864676114bc56cef51eacce317b1132ebe712c11
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-04-26 01:26:21 -0300

    Criando a regra de negocio para seleção das frases

--- index.android.js
+++ index.android.js
@@ -21,6 +21,17 @@ const Style = {
 };
 
 const buttonPressed = () => {
+  let randomNumber = Math.random();
+  randomNumber = Math.floor(randomNumber * 5);
+
+  let phrases = Array();
+  phrases[0] = 'A persistência é o caminho do êxito.';
+  phrases[1] = 'Se algo é difícil de fazer, então não vale a pena ser feito!';
+  phrases[2] = 'A culpa é minha e eu coloco ela em quem eu quiser!';
+  phrases[3] = 'Álcool... A causa e solução de todos os problemas.';
+  phrases[4] = 'Eu não estava mentindo! Estava escrevendo ficção com a boca.';
+
+  let phraseSelected = phrases[randomNumber];
 
 }
 
@@ -31,7 +42,7 @@ const App = () => {
     <View style={main}>
       <Image source={require('./imgs/logo.png')}/>
 
-      <TouchableOpacity style={button}>
+      <TouchableOpacity style={button} onPress={buttonPressed}>
         <Text style={buttonText}>Nova Frase</Text>
       </TouchableOpacity>
     </View>
